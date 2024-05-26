26794b831c242c98c4913650813acd0c5d06eed2
sancheet48 <sancheet8baidya@gmail.com>
2024-05-26 18:38:48 +0530

    Added my first component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MyFirstComponentComponent } from './my-first-component/my-first-component.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MyFirstComponentComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'angular-crash-course';
+  title = 'Sancheet';
 }
